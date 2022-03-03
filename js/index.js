bc84cc967a2ca4f86d7563282bcc2cf26cad88e8
pretzL <ericpretzinger@gmail.com>
2022-03-03 19:46:32 +0100

    attempted coming soon API, not successful

--- js/index.js
+++ js/index.js
@@ -32,3 +32,39 @@ async function getGames() {
 }
 
 getGames();
+
+const URL = "https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added?key=35f9fd70b7b54c25bfa1662ebdeaff60";
+
+const corsFix = "https://noroffcors.herokuapp.com/" + URL;
+
+const comingSoonContainer = document.querySelector(".content-coming-soon");
+
+async function getUpcomingGames() {
+  try {
+    const response = await fetch(corsFix);
+    const results = await response.json();
+    console.log(results);
+
+    const games = results.results;
+
+    hotContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+      if (i === 4) {
+        break;
+      }
+
+      comingSoonContainer.innerHTML += `<a href="/details.html?id=${games[i].id}" class="card">
+        <img src="${games[i].background_image}" class="card-image"/>
+          <h3>${games[i].name}</h3>
+          <p>Rating: ${games[i].rating}</p>
+          <p>Released: ${games[i].released}</p>
+          </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+getUpcomingGames();
