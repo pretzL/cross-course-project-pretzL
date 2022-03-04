5676f9a21f420d70f7df50983de8c299f07db039
pretzL <ericpretzinger@gmail.com>
2022-03-04 12:05:29 +0100

    finished hot, comingsoon and marketplace front page categories

---
+++ js/comingsoon4.js
@@ -0,0 +1,33 @@
+const URL = "https://api.rawg.io/api/games?key=35f9fd70b7b54c25bfa1662ebdeaff60";
+
+const comingSoonContainer = document.querySelector(".content-coming-soon");
+
+async function getUpcomingGames() {
+  try {
+    const response = await fetch(URL);
+    const results = await response.json();
+    console.log(results);
+
+    const games = results.results;
+
+    hotContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+      if (i === 4) {
+        break;
+      }
+
+      comingSoonContainer.innerHTML += `<a href="/details.html?id=${games[i].id}" class="card">
+        <img src="${games[i].background_image}" class="card-image"/>
+          <h3>${games[i].name}</h3>
+          <p>Rating: ${games[i].rating}</p>
+          <p>Released: ${games[i].released}</p>
+          </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+getUpcomingGames();
---
+++ js/marketplace4.js
@@ -0,0 +1,33 @@
+const marketplaceURL = "https://api.rawg.io/api/games?key=35f9fd70b7b54c25bfa1662ebdeaff60";
+
+const marketplaceContainer = document.querySelector(".content-marketplace");
+
+async function getMarketplaceGames() {
+  try {
+    const response = await fetch(marketplaceURL);
+    const results = await response.json();
+    console.log(results);
+
+    const games = results.results;
+
+    marketplaceContainer.innerHTML = "";
+
+    for (let i = 4; i < games.length; i++) {
+      if (i === 8) {
+        break;
+      }
+
+      marketplaceContainer.innerHTML += `<a href="/details.html?id=${games[i].id}" class="card">
+      <img src="${games[i].background_image}" class="card-image"/>
+        <h3>${games[i].name}</h3>
+        <p>Rating: ${games[i].rating}</p>
+        <p>Released: ${games[i].released}</p>
+        </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+getMarketplaceGames();
