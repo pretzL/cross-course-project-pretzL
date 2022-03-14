a59db3eadf20485eafdafa71752d296d217053ba
pretzL <ericpretzinger@gmail.com>
2022-03-14 21:22:49 +0100

    added purchase history and owned games to user profile

--- js/user-profile.js
+++ js/user-profile.js
@@ -14,3 +14,53 @@ if (username === null) {
   headingOne.innerHTML = username + "`s profile";
   usernameText.innerHTML = username;
 }
+
+const URL = "https://api.rawg.io/api/games";
+
+const key = "?key=35f9fd70b7b54c25bfa1662ebdeaff60";
+
+const ownedGamesContainer = document.querySelector(".user-owned-games-content");
+const purchasedGamesContainer = document.querySelector(".user-purchased-games-content");
+
+async function getGames() {
+  try {
+    const response = await fetch(URL + key);
+    const results = await response.json();
+
+    const games = results.results;
+
+    ownedGamesContainer.innerHTML = "";
+    purchasedGamesContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+      if (i === 3) {
+        break;
+      }
+
+      ownedGamesContainer.innerHTML += `<a href="/game-profile.html?id=${games[i].id}" class="card">
+          <img src="${games[i].background_image}" class="card-image" alt="${games[i].name}"/>
+            <h3>${games[i].name}</h3>
+            <p>Rating: ${games[i].rating}</p>
+            <p>Released: ${games[i].released}</p>
+            </a>`;
+    }
+
+    for (let i = 3; i < games.length; i++) {
+      if (i === 6) {
+        break;
+      }
+
+      purchasedGamesContainer.innerHTML += `<a href="/game-profile.html?id=${games[i].id}" class="card">
+          <img src="${games[i].background_image}" class="card-image" alt="${games[i].name}"/>
+            <h3>${games[i].name}</h3>
+            <p>Rating: ${games[i].rating}</p>
+            <p>Released: ${games[i].released}</p>
+            </a>`;
+    }
+  } catch (error) {
+    console.log(error);
+    errorContainer.innerHTML = errorMessage("An error occurred when calling the API, error: " + error);
+  }
+}
+
+getGames();
