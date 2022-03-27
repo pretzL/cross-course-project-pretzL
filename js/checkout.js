3686657b769dee952931675a325ab8148a40b27f
pretzL <ericpretzinger@gmail.com>
2022-03-27 21:21:16 +0200

    fixed cart items passing on to checkout page

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,6 @@
+import { getExistingCart } from "./components/cartFunctions.js";
+import { cartItemsLength } from "./components/cartFunctions.js";
+
 // Form tutorial by Web Dev Simplified (YouTube)
 
 const multiStepForm = document.querySelector("[data-multi-step]");
@@ -59,3 +62,51 @@ function noSubmit(form) {
 }
 
 multiStepForm.addEventListener("submit", noSubmit);
+
+// CART LOCAL STORAGE
+
+const checkoutContainer = document.querySelector(".checkout-container");
+const itemTotal = document.querySelector(".item-total-price");
+
+const carts = getExistingCart();
+
+if (carts.length === 0) {
+  checkoutContainer.innerHTML = `<p class="cart-error">You have no games in your cart.</p>`;
+}
+
+let itemNumber = 1;
+
+let price = 19.94;
+
+let priceTotal = price * itemNumber;
+
+let calculatedPriceTotal = priceTotal * carts.length;
+
+for (let i = 0; i < carts.length; i++) {
+  checkoutContainer.innerHTML += `
+      <div class="checkout-item">
+        <p class="">${carts[i].name}</p>
+        <div class="price-icon-container">
+          <p class="item-price">$ ${priceTotal}</p>
+          <div class="item-trashcan"><span class="material-icons"> delete </span></div>
+        </div>
+      </div>`;
+}
+
+itemTotal.innerHTML = "Total: $" + calculatedPriceTotal.toFixed(2);
+
+/* <h3 class="">Item</h3>
+                  <div class="checkout-item-one">
+                    <p>The Elder Scrolls V: Skyrim</p>
+                  </div>
+                  <div class="checkout-item-two">
+                    <p>Raft</p>
+                  </div>
+                </div>
+                <div class="checkout-price checkout-grid3">
+                  <h3>Price</h3>
+                  <div class="price-list">
+                    <p class="item-one-price">$40.83</p>
+                    <p class="item-two-price">$19.94</p>
+                  </div>
+*/
